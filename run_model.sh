0941439a3600ac4f3a672cd30ea94a3b3d30c844
amorimEduardoo <eduardo_pontes130@hotmail.com>
2022-09-13 10:53:37 -0300

    update production file

--- run_model.sh
+++ run_model.sh
@@ -2,6 +2,6 @@
 data=$(date +'%Y-%m-%dT%H:%M:%S')
 
 #path
-path='/home/ed/Downloads/Insider_Clustering'
+path='/home/ed/Downloads/Insiders_program'
 path_to_envs='/home/ed/anaconda3/bin'
 $path_to_envs/papermill $path/src/models/c10.0_deploy.ipynb $path/reports/c10.0_deploy_$data.ipynb
